e7d2757dbb0263a67435e1e567b1bc45a37b1f01
monica-suarez <shutterbugmo@gmail.com>
2020-10-19 21:29:17 -0500

    got handleclick functioning

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,10 +1,45 @@
 import React from 'react'
+import {Button, 
+        Table, 
+        TableHead, 
+        TableBody, 
+        TableRow, 
+        TableCell
+        } from '@material-ui/core'
 
 const Import = (props) => {
     // fill out this component
 
     return (
-        <p>Import Component</p>
+            <div>
+            <Button onClick={props.fetchMakes} variant="contained" color="primary">Import</Button>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>
+                            ID
+                        </TableCell>
+                        <TableCell>
+                            Makes
+                        </TableCell>
+                        <TableCell>
+                            Actions
+                        </TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {props.makes.map(make => {
+                    return  <TableRow>
+                                <TableCell>{make.MakeId}</TableCell>
+                                <TableCell>{make.MakeName}</TableCell>
+                                <TableCell></TableCell>
+                            </TableRow>
+                    })}
+        
+
+                </TableBody>
+            </Table>
+            </div>
     )
 }
 
